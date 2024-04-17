761faf29f55ff1db63a383db23ca765c4d68ed66
DHEBP <dhebp@proton.me>
2024-04-16 21:04:22 -0500

    improved metadata

--- seo.config.ts
+++ seo.config.ts
@@ -9,18 +9,18 @@ const seoConfig = defineMetadata({
     default:
       'DERO is a privacy-focused decentralized application platform.'
   },
-  description: 'DERO a platform for developers to deploy secure, open, scalable, and privacy-preserving decentralized applications',
+  description: 'DERO is a privacy-focused decentralized application platform.',
   themeColor: '#061636',
   openGraph: {
-    images: '/og-image.png',
+    images: 'assets/og-image.png',
     url: 'https://dero.is'
   },
-  manifest: '/site.webmanifest',
+  manifest: 'assets/site.webmanifest',
   icons: [
-    { rel: 'icon', url: '/favicon.ico' },
-    { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' },
-    { rel: 'mask-icon', url: '/favicon.ico' },
-    { rel: 'image/x-icon', url: '/favicon.ico' }
+    { rel: 'icon', url: 'assets/favicon.ico' },
+    { rel: 'apple-touch-icon', url: 'assets/apple-touch-icon.png' },
+    { rel: 'mask-icon', url: 'assets/favicon.ico' },
+    { rel: 'image/x-icon', url: 'assets/favicon.ico' }
   ],
   twitter: {
     site: '@DERO_Foundation',
